43103a70523e705b51e2bc20ec238b69ceed318e
Matt Collins <matt.collins087@gmail.com>
2022-09-14 11:56:21 -0700

    updated firbase and signin functions to save users to DB

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -12,7 +12,7 @@ const SignIn = () => {
   return (
     <div>
       <h1>Sign in Page</h1>
-      <button onClick={logGoogleUser}>Sign in with Google</button>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
     </div>
   );
 };
