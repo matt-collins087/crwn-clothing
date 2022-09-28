fb4071df0c63ad1f9bf700c2788d83e6b2246273
Matt Collins <matt.collins087@gmail.com>
2022-09-28 10:02:38 -0700

    adding cart context and shopping cart icon aswell as cart dropdown components

--- src/App.js
+++ src/App.js
@@ -2,15 +2,8 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
-import Authentication from "./routes/authentication/authentication.component"
-
-const Shop = () => {
-  return (
-    <div>
-      <h1>I am the shop page</h1>
-    </div>
-  );
-};
+import Authentication from "./routes/authentication/authentication.component";
+import Shop from "./routes/shop/shop.component";
 
 const App = () => {
   return (
---
+++ src/components/product-card/product-card.component.jsx
@@ -0,0 +1,20 @@
+import "./product-card.styles.scss";
+
+import Button from "../button/button.component";
+
+const ProductCard = ({product}) => {
+    const {imageUrl, name, price} = product;
+
+    return (
+        <div className="product-card-container">
+            <img alt={`${name}`} src={imageUrl}/>
+            <div className="footer">
+              <span className="name">{name}</span>  
+              <span className="price">{price}</span>
+            </div>
+            <Button buttonType="inverted">Add to Cart</Button>
+        </div>
+    )
+}
+
+export default ProductCard;
